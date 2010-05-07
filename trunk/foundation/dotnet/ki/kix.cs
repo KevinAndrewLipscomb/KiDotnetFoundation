cb737c274447959108ce0d1f449dd6f9c1b40aa7
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-05-07 18:09:22 +0000

    Upgraded to Visual Web Developer 2010 Express.  Added int_sign_range.

--- trunk/foundation/dotnet/ki/kix.cs
+++ trunk/foundation/dotnet/ki/kix.cs
@@ -243,6 +243,26 @@ namespace kix
             }
           }
 
+        public class int_sign_range
+          {
+          private subtype<int> current;
+          public int_sign_range()
+            {
+            current = new subtype<int>(-1,1);
+            }
+          public int val
+            {
+            get
+              {
+              return current.val;
+              }
+            set
+              {
+              current.val = value;
+              }
+            }
+          }
+
         public class decimal_nonnegative
           {
           private subtype<decimal> current;
