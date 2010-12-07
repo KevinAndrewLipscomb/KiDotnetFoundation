b9a04acb09b9cbdf6eb96da331a32cede32385b3
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-12-07 17:40:08 +0000

    Added code to ki_web_ui to guard against lost session state.  Revised kix to include additional guidance for IIS7.

--- trunk/foundation/dotnet/ki/kix.cs
+++ trunk/foundation/dotnet/ki/kix.cs
@@ -1038,7 +1038,7 @@ namespace kix
         public static void SendControlAsAttachmentToEmailMessage(object c, string scratch_pathname, string from_address, string to_target, string cc_target, string subject, string body)
         {
             //
-            // For this to work, the ASP.NET Machine Account (ASPNET) must have write permission for the folder specified by p.path.  Configure this on the Security tab of the folder's Properties.  If the Security tab is missing, open Windows Explorer / Tools /
+            // For this to work, the ASP.NET Machine Account (ASPNET) [on IIS5] or the NETWORK SERVICE account [on IIS7] must have write permission for the folder specified by p.path.  Configure this on the Security tab of the folder's Properties.  If the Security tab is missing, open Windows Explorer / Tools /
             // Folder Options... / View, and in the Advanced Settings, clear the "Use simple file sharing" checkbox.
             //
             System.IO.StreamWriter streamwriter = new System.IO.StreamWriter(scratch_pathname);
