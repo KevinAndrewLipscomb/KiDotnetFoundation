6612f82419ab70a7226d59e66b2846ac9cdfc360
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2013-01-03 00:04:11 +0000

    Revised to use newer version of Google Font Loader.

--- trunk/foundation/dotnet/ki/ki_web_ui.cs
+++ trunk/foundation/dotnet/ki/ki_web_ui.cs
@@ -201,7 +201,7 @@ namespace ki_web_ui
             + " (function ()"
             +   " {"
             +   " var wf = document.createElement('script');"
-            +   " wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';"
+            +   " wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.1.0/webfont.js';"
             +   " wf.type = 'text/javascript';"
             +   " wf.async = 'true';"
             +   " var s = document.getElementsByTagName('script')[0];"
