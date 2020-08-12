b95604c2f4aca0caf4031cef879dca83e52f454a
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2020-08-12 23:37:36 +0000

    Revised to handle disallowed (hacker-like) text field input gracefully.

--- trunk/foundation/dotnet/ki/php_app_module.cs
+++ trunk/foundation/dotnet/ki/php_app_module.cs
@@ -1,5 +1,6 @@
 ﻿using kix;
 using System;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace ki
@@ -30,23 +31,32 @@ namespace ki
     public void OnError(Object source, EventArgs e)
       {
       var http_application_source = (source as HttpApplication);
-      var request_user_host_address = http_application_source.Request.UserHostAddress;
-      var request_url_string = http_application_source.Request.Url.ToString();
-      k.EscalatedException
-        (
-        the_exception:http_application_source.Server.GetLastError(),
-        user_identity_name:k.EMPTY
-        + "Someone at " + request_user_host_address + " (" + k.DomainNameOfIpAddress(request_user_host_address) + ")" + k.NEW_LINE
-        + "visiting " + request_url_string + k.NEW_LINE
-        + k.WrapText
-            (
-            t:http_application_source.Request.Form.ToString(),
-            insert_string:k.SPACE + k.SPACE,
-            break_char_array:"&".ToCharArray(),
-            max_line_len:short.MaxValue
-            )
-        );
-      http_application_source.Response.Redirect(url:"~/exception.phtml");
+      var last_error = http_application_source.Server.GetLastError();
+      //
+      if (Regex.IsMatch(last_error.ToString(),"A potentially dangerous Request.* value was detected from the client", RegexOptions.IgnoreCase))
+        {
+        http_application_source.Response.Redirect(url:"~/bad-input.phtml");
+        }
+      else
+        {
+        var request_user_host_address = http_application_source.Request.UserHostAddress;
+        var request_url_string = http_application_source.Request.Url.ToString();
+        k.EscalatedException
+          (
+          the_exception:last_error,
+          user_identity_name:k.EMPTY
+          + "Someone at " + request_user_host_address + " (" + k.DomainNameOfIpAddress(request_user_host_address) + ")" + k.NEW_LINE
+          + "visiting " + request_url_string + k.NEW_LINE
+          + k.WrapText
+              (
+              t:http_application_source.Request.Form.ToString(),
+              insert_string:k.SPACE + k.SPACE,
+              break_char_array:"&".ToCharArray(),
+              max_line_len:short.MaxValue
+              )
+          );
+        http_application_source.Response.Redirect(url:"~/exception.phtml");
+        }
       }
     }
   }
